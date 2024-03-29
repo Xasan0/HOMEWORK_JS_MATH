9879b200bd5b4b76592a4c8cb05c8bedc6cc76ea
Xasan0 <xasanceek0733@gmail.com>
2024-03-29 13:54:33 +0500

    added all

---
+++ index.js
@@ -0,0 +1,104 @@
+//2. O'girlik M kilogramda berilgan. Undagi to'liq tonnalar sonini aniqlovchi programma tuzilsin. (1t=1000kg)
+
+//let kg=+prompt('Kg:');
+//let t=kg/1000;
+
+//alert(t+' tonna');
+
+
+//3. Faylning hajmi baytlarda berilgan. Bo'lib tunni olish operatsiyasidan fayl hajmining kilobaytlarda programma tuzilsin.
+
+// let byte=+prompt('Byte:');
+// let a=byte%1024;
+// let kb=(byte-a)
+
+// alert(kb+ ' kilobite')
+
+
+// 4. A va B (A > B) musbat sonlari berilgan. A kesmada, B kesmani nechi marta joylashtirish mumkunligini aniqlovchi programma tuzilsin.
+
+// let a=+prompt("A sonini kiriting: ")
+// let b=+prompt("B sonini kiriting: ")
+
+// let n=a/b
+
+// alert(n+ " javob")
+
+// 5.A va B (A > B) musbat sonlari berilgan. A kesmada, B kesmani nechi marta joylashtirish mumkun. A kesmada B kesmaning  joylashgan qismini aniqlovchi programma tuzilsin 
+
+// let a=+prompt("A sonini kiriting: ")
+// let b=+prompt("B sonini kiriting: ")
+
+// let n=a%b
+
+// alert(n+ " javob")
+
+// 15.misol
+// let son = 247;
+// let birliklar = son % 10; 
+// let yuzliklar = (son / 100); 
+
+// let hosilSon = birliklar * 100 + yuzliklar * 10 + son % 100 - birliklar - yuzliklar * 100;
+
+// console.log("Berilgan son: " + son);
+// console.log("Almashtirilgan son: " + hosilSon);
+
+// 16.misol
+ 
+// let son = 365;  
+// let oNliklar = (son / 10) % 10;  
+// let birliklar = son % 10;  
+
+// let hosilSon = oNliklar * 10 + birliklar;  
+
+// console.log("Berilgan son: " + son);
+// console.log("Almashtirilgan son: " + hosilSon);
+
+// 17.misol
+
+// let son = 1234;  
+// let yuzliklar = Math.floor(son / 100) % 10;  
+
+// console.log("Berilgan son: " + son);
+// console.log("Yuzliklar xonasidagi raqam: " + yuzliklar);
+
+// 18.misol
+
+// let son = 123456; 
+// let mingliklar = Math.floor(son / 1000) % 10; 
+
+// console.log("Berilgan son: " + son);
+// console.log("Mingliklar xonasidagi raqam: " + mingliklar);
+
+
+// 19.misol
+
+// let sekundlar = 3600; 
+// let minutlar = Math.floor(sekundlar / 60); 
+
+// console.log("Sekundlar: " + sekundlar);
+// console.log("Minutlar: " + minutlar);
+
+
+// 20.misol
+
+// let sekundlar = 7200; 
+// let soatlar = Math.floor(sekundlar / 3600); 
+
+// console.log("Sekundlar: " + sekundlar);
+// console.log("Soatlar: " + soatlar);
+
+
+// -----------------------------------------------------------------------
+
+
+// let userEmail = "xasanceek0733@gmail.com"
+// let userPassword = "qwerty123"
+
+// if(userEmail === "xasanceek0733@gmail.com" && userPassword === "qwerty123") {
+//     console.log("Welcome !")
+// }
+
+// else {
+//     console.log ("Something went wrong")
+// }
